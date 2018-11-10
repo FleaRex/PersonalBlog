348bd51b6ac3f3f1b4e83a424af7874b61d9b0d8
Ben Madley <bmad29@hotmail.co.uk>
2018-11-10 20:08:14 +0000

    Adding captioned images.

---
+++ _plugins/PictureTag.rb
@@ -0,0 +1,21 @@
+class PictureTag < Liquid::Tag
+  def initialize(tag_name, input, tokens)
+    super
+    @input = input
+  end
+
+  def render(context)
+    input_split = @input.split('|')
+    caption = input_split[0].strip
+    image = input_split[1].strip
+
+    output =  <<-PIC
+<div class="captionedImage">
+  <img src="#{image}" alt="#{caption}" width="100%">
+  <p class="caption">#{caption}</p>
+</div>
+    PIC
+    return output;
+  end
+end
+Liquid::Template.register_tag('picture', PictureTag)
